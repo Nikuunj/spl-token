6aa253f161ba6f69e424d1a54fefa46bd7476da1
Nikunj <mnikunj262@gmail.com>
2026-01-29 00:35:17 +0530

    update: readme

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use solana_client::nonblocking::rpc_client::RpcClient;
+use solana_commitment_config::CommitmentConfig;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let client = RpcClient::new_with_commitment(
+        String::from("https://api.testnet.solana.com"),
+        CommitmentConfig::confirmed(),
+    );
+
+    let latest_blockhash = client.get_latest_blockhash().await?;
+
+    print!("{}", latest_blockhash);
+
+    Ok(())
 }
