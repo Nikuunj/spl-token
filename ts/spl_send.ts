f659cd6e415b627b6951cf00048ea32c4ff42ec3
Nikunj <mnikunj262@gmail.com>
2026-01-28 23:07:28 +0530

    update: transfer token 2022 done

--- ts/spl_send.ts
+++ ts/spl_send.ts
@@ -9,7 +9,7 @@ const commitement: Commitment = "confirmed";
 const connection = new Connection("https://api.testnet.solana.com", commitement);
 
 const mint = new PublicKey("59xzWKF8LbmGweS1TU91F2T8o1wnUjFfThsrCNdMU8nB");
-const pub = new PublicKey("2FvMsMY1DKpnJL3FUk4HBPVffzK4iF55rZHKPNgikef1");
+const pub = new PublicKey("C81XCoS1VToRzYYQ35N5Afow5iTRsZmZLrpXdYV61eeZ");
 
 async function main() {
    const ataSender = getAssociatedTokenAddressSync(mint, keypair.publicKey, false, TOKEN_2022_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID);
@@ -38,24 +38,16 @@ async function main() {
    // console.log(ataAcc);
    
    
-   if(!ataAcc) {
-      const tx = new Transaction();
-      tx.add(createAtaAccTx);
-      tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-      tx.feePayer = keypair.publicKey;
-      const sign = await connection.sendTransaction(tx, [keypair]);
-
-      console.log("ATA for rec : ",sign);
-  
-   }
    const tx = new Transaction();
-   tx.add(sendTokenTx);
-
+   if(!ataAcc) {
+      tx.add(createAtaAccTx, sendTokenTx);
+   } else {
+      tx.add(sendTokenTx);
+   }
    tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
    tx.feePayer = keypair.publicKey;
-
    const sign = await connection.sendTransaction(tx, [keypair]);
-
+   
    console.log("Transfer Token : ", sign);
    
 }
